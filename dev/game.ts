60724b799ee285424de36caa92da26bed558116e
koen-benne <57910795+koen-benne@users.noreply.github.com>
2020-05-11 16:41:48 +0200

    Updated game.ts

--- dev/game.ts
+++ dev/game.ts
@@ -1,7 +1,10 @@
 let pixelWidth = 2;
+/// <reference path="player.ts"/>
 class Game {
+    public playerOne : Player = new Player();
+    public playerTwo : Player = new Player();
     // Set game variables
-    public currentScreen : any;
+    public currentScene : any;
 
     // Constructor
     constructor() {
@@ -10,22 +13,23 @@ class Game {
     }
 
     private gameLoop() {
-
-        this.currentScreen.update();
-        requestAnimationFrame(() => this.gameLoop());
+        this.currentScene.update().then(
+            () => requestAnimationFrame(() => this.gameLoop())
+        );
     }
 
     private onStart() {
-
+        this.setPlayScreen();
     }
 
     public setStartScreen() {
         document.body.innerHTML = "";
-        this.currentScreen = new StartScreen(this);
+        this.currentScene = new StartScreen(this);
     }
 
     public setPlayScreen() {
         document.body.innerHTML = "";
-        this.currentScreen = new PlayScreen(this);
+        this.currentScene = new PlayScreen(this);
     }
 }
+window.addEventListener("load", () => new Game());
